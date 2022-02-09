8f57b3753ef64588582287d23498f389db1613d3
Eric Donnelly <ericdonnelly4@gmail.com>
2022-02-08 22:43:33 -0500

    Delete globals.js

--- config/globals.js
+++
@@ -1,5 +0,0 @@
-const configurations = {
-    'db': 'mongodb+srv://admin:LQwox6eVw53honqL@cluster0.tmyya.mongodb.net/LifeReframed?retryWrites=true&w=majority',
-    'sessionSecret':'L1f3R3f4mED'
-}
-module.exports =configurations ;
