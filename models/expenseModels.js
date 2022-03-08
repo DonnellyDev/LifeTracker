661e980a9afb3223f3f9475c3cdf9364a4a6dca6
Ridhish Kanara <200440526@student.georgianc.on.ca>
2022-03-08 16:12:04 -0500

    edited models and made Hbs files #1xcwwnv

--- models/expenseModels.js
+++ models/expenseModels.js
@@ -1,11 +1,33 @@
 // Import mongoose
 const mongoose = require('mongoose');
 
+
 // Create schema definition object
 const expenseSchemaDefiniton = {
+    date:{
+        type:Date,
+        required:true
+    },
+    account:{
+        type:String,
+        required :true
+    },
+
     description: {
         type: String,
         required: true
+    },
+    amount:{
+      type: Number,
+      required: true
+    },
+    note: {
+      type: String,
+      required: true
+    },
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
     }
 };
 
